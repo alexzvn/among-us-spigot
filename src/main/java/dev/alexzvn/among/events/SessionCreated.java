e68c4af3884cf3d7a4bedafb258ef558685ead9c
Alex <41188285+alexzvn@users.noreply.github.com>
2020-10-26 10:11:51 +0000

    add event

---
+++ src/main/java/dev/alexzvn/among/events/SessionCreated.java
@@ -0,0 +1,33 @@
+package dev.alexzvn.among.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import dev.alexzvn.among.contract.Session;
+
+public class SessionCreated extends Event {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    protected String id;
+
+    protected Session session;
+
+    public SessionCreated(String id, Session session) {
+        this.id = id;
+        this.session = session;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+}
