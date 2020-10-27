0ea44570d127bcafe233d8fd9d927194009b5d10
Alex <41188285+alexzvn@users.noreply.github.com>
2020-10-27 06:33:45 +0000

    update world copier

--- src/main/java/dev/alexzvn/among/util/WorldMap.java
+++ src/main/java/dev/alexzvn/among/util/WorldMap.java
@@ -7,50 +7,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
 public class WorldMap {
-    private static void copyFileStructure(File source, File target){
 
-        ArrayList<String> ignore = new ArrayList<String>();
-
-        ignore.add("uid.dat");
-        ignore.add("session.lock");
-
-        try {
-           
-            if(!ignore.contains(source.getName())) {
-                if(source.isDirectory()) {
-                    if(!target.exists())
-                        if (!target.mkdirs())
-                            throw new IOException("Couldn't create world directory!");
-                    String files[] = source.list();
-                    for (String file : files) {
-                        File srcFile = new File(source, file);
-                        File destFile = new File(target, file);
-                        copyFileStructure(srcFile, destFile);
-                    }
-                } else {
-                    InputStream in = new FileInputStream(source);
-                    OutputStream out = new FileOutputStream(target);
-                    byte[] buffer = new byte[1024];
-                    int length;
-                    while ((length = in.read(buffer)) > 0)
-                        out.write(buffer, 0, length);
-                    in.close();
-                    out.close();
+    public static void teleAsyncPlayerToLobby(Player player) {
+        Bukkit.getScheduler()
+            .runTaskAsynchronously(Plugin.getPlugin(), new Runnable(){
+                @Override
+                public void run() {
+                    telePlayerToLobby(player);
                 }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            });
+    }
+
+    public static void telePlayerToLobby(Player player) {
+        player.teleport(getLobbyLocation(), TeleportCause.PLUGIN);
     }
 
     public static boolean unload(World world) {
-        return world!=null && Bukkit.getServer().unloadWorld(world, false);
+        return world != null && Bukkit.getServer().unloadWorld(world, false);
     }
 
     public static World copy(World origin, String newName) {
@@ -60,5 +44,81 @@ public class WorldMap {
         );
 
         return new WorldCreator(newName).createWorld();
+    }
+
+    public static void delete(World world) {
+
+        Bukkit.getScheduler().runTaskAsynchronously(Plugin.getPlugin(), new Runnable(){
+
+            @Override
+            public void run() {
+                if (unload(world)) {
+                    new File(Bukkit.getWorldContainer(), world.getName()).delete();
+                }
+            }
+        });
+    }
+
+    public static Location getLobbyLocation() {
+        FileConfiguration config = Plugin.config();
+
+        World world = Bukkit.getWorld(
+            config.getString("lobby.location.name", "world")
+        );
+
+        for (String dimension : new String[] {"x", "y", "z"}) {
+            if (! config.isSet("lobby.location.".concat(dimension))) {
+                return world.getSpawnLocation();
+            }
+        }
+
+        return new Location(
+            world,
+            config.getInt("lobby.location.x"),
+            config.getInt("lobby.location.y"),
+            config.getInt("lobby.location.z")
+        );
+    }
+
+    private static void copyFileStructure(File source, File target){
+
+        ArrayList<String> ignore = new ArrayList<String>(Arrays.asList(
+            "uid.dat",
+            "session.lock"
+        ));
+
+        try {
+            if(ignore.contains(source.getName())) return;
+
+            if(source.isDirectory()) {
+                createWorldDir(source, target); return;
+            }
+
+            InputStream in = new FileInputStream(source);
+            OutputStream out = new FileOutputStream(target);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0)
+                out.write(buffer, 0, length);
+            in.close();
+            out.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void createWorldDir(File source, File target) throws IOException {
+        if(!target.exists())
+            if (!target.mkdirs())
+                throw new IOException("Couldn't create world directory!");
+
+        String files[] = source.list();
+
+        for (String file : files) {
+            File srcFile = new File(source, file);
+            File destFile = new File(target, file);
+            copyFileStructure(srcFile, destFile);
+        }
     }
 }
