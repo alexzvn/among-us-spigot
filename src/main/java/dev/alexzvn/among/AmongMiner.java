cad399501c090f3ef9a5473ed9a173f98f17492d
Alex <41188285+alexzvn@users.noreply.github.com>
2020-10-27 06:34:18 +0000

    update command & listener

--- src/main/java/dev/alexzvn/among/AmongMiner.java
+++ src/main/java/dev/alexzvn/among/AmongMiner.java
@@ -3,9 +3,13 @@ package dev.alexzvn.among;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import dev.alexzvn.among.commands.*;
+import dev.alexzvn.among.commands.AmongMinerCommand;
+import dev.alexzvn.among.commands.Join;
+import dev.alexzvn.among.commands.Leave;
 import dev.alexzvn.among.session.SessionManager;
 import dev.alexzvn.among.util.Plugin;
 import dev.alexzvn.among.util.Util;
@@ -27,8 +31,8 @@ public class AmongMiner extends JavaPlugin {
         // Load static config
         new Plugin(this);
 
-        //register command
         this.registerCommand();
+        this.registerListener();
     }
 
     @Override
@@ -38,8 +42,18 @@ public class AmongMiner extends JavaPlugin {
     }
 
     protected void registerCommand() {
-        this.getCommand("amongminer").setExecutor(new MainHandle());
+        this.getCommand("amongminer").setExecutor(new AmongMinerCommand());
         this.getCommand("join").setExecutor(new Join());
         this.getCommand("leave").setExecutor(new Leave());
+    }
+
+    protected void registerListener() {
+        Listener[] listeners = {
+            
+        };
+
+        for (Listener listener : listeners) {
+            Bukkit.getPluginManager().registerEvents(listener, this);
+        }
     }
 }
