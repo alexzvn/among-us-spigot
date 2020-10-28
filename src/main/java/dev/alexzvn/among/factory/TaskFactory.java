bbdb6c1c70498060f82637571da922096696e2ce
Alex <41188285+alexzvn@users.noreply.github.com>
2020-10-28 15:50:08 +0000

    add task factory

---
+++ src/main/java/dev/alexzvn/among/factory/TaskFactory.java
@@ -0,0 +1,19 @@
+package dev.alexzvn.among.factory;
+
+import dev.alexzvn.among.contract.game.Task;
+import dev.alexzvn.among.contract.team.Crewmate;
+
+public class TaskFactory {
+    
+    public static Task createShortTask(Crewmate crewmate) {
+        return null;
+    }
+
+    public static Task createCommonTask(Crewmate crewmate) {
+        return null;
+    }
+
+    public static Task createLongTask(Crewmate crewmate) {
+        return null;
+    }
+}
