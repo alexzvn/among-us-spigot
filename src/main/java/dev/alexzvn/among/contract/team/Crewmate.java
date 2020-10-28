ad87edfa3e212d3caf09ebd0c408776f229a2694
Alex <41188285+alexzvn@users.noreply.github.com>
2020-10-28 15:49:23 +0000

    update contract cewmate

--- src/main/java/dev/alexzvn/among/contract/team/Crewmate.java
+++ src/main/java/dev/alexzvn/among/contract/team/Crewmate.java
@@ -2,17 +2,24 @@ package dev.alexzvn.among.contract.team;
 
 import org.bukkit.entity.Player;
 
-import dev.alexzvn.among.contract.Session;
+import dev.alexzvn.among.contract.game.RoomSession;
+import dev.alexzvn.among.contract.game.Task;
 
 public interface Crewmate {
 
     public Player getPlayer();
 
-    public Session getSession();
+    public RoomSession getSession();
 
-    public void doTask();
+    public void doTask(Task task);
 
     public void reportBody();
 
     public void callEmergency();
+
+    public void setPlayerDead();
+
+    public void setEmergencyMeetings(int emergencyMeetings);
+
+    public boolean isAlive();
 }
--- src/main/java/dev/alexzvn/among/contract/team/Imposter.java
+++ src/main/java/dev/alexzvn/among/contract/team/Imposter.java
@@ -1,12 +1,14 @@
 package dev.alexzvn.among.contract.team;
 
+import dev.alexzvn.among.contract.game.Task;
+
 public interface Imposter {
 
     public void kill(NonImposter crewmate);
 
     public void vent();
 
-    public void fakeTask();
+    public void fakeTask(Task Task);
 
     public void sabotage();
 }
