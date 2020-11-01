2940a12e00f62c35041cec01993c23d4dd361f8f
Alex <41188285+alexzvn@users.noreply.github.com>
2020-11-01 10:34:41 +0000

    add leave command

--- src/main/java/dev/alexzvn/among/commands/Leave.java
+++ src/main/java/dev/alexzvn/among/commands/Leave.java
@@ -3,13 +3,23 @@ package dev.alexzvn.among.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import dev.alexzvn.among.game.session.SessionManager;
 
 public class Leave implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // TODO Auto-generated method stub
-        return false;
+        if (! (sender instanceof Player)) {
+            return false;
+        }
+       
+        if (SessionManager.hasPlayer((Player) sender)) {
+            SessionManager.disconnectPlayer((Player) sender);
+        }
+
+        return true;
     }
     
 }
